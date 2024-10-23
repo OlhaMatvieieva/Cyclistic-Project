aad9cabd8cfe5b6d91c2876bc6c72439034f874d
OlhaMatvieieva <162842375+OlhaMatvieieva@users.noreply.github.com>
2024-10-23 15:32:05 +0200

    Update Data Cleaning.sql

--- Data Cleaning.sql
+++ Data Cleaning.sql
@@ -1,57 +1,81 @@
 -- Data Cleaning
 
--- checking for number of null values
+-- remove rows with null values 
+-- delete duplicates and rows from columns started_at, ended_at ride duration which are less one minute and more than one day
+-- create the columns 'month and' 'day_of week' where 1 represents Sunday, 7 represents Monday 
+-- determine the ride length in minutes 
 
-SELECT COUNT(*) - COUNT(ride_id) ride_id
-  ,COUNT(*) - COUNT(rideable_type) rideable_type
-  ,COUNT(*) - COUNT(started_at) started_at
-  ,COUNT(*) - COUNT(ended_at) ended_at
-  ,COUNT(*) - COUNT(start_station_name) start_station_name
-  ,COUNT(*) - COUNT(start_station_id) start_station_id
-  ,COUNT(*) - COUNT(end_station_name) end_station_name
-  ,COUNT(*) - COUNT(end_station_id) end_station_id
-  ,COUNT(*) - COUNT(start_lat) start_lat
-  ,COUNT(*) - COUNT(start_lng) start_lng
-  ,COUNT(*) - COUNT(end_lat) end_lat
-  ,COUNT(*) - COUNT(end_lng) end_lng
-  ,COUNT(*) - COUNT(member_casual) member_casual
-FROM `dotted-forest-437921-f3.Cyclistic.join_data`;
-
--- checking for duplicate rows
-
-SELECT COUNT(ride_id) - COUNT(DISTINCT ride_id) AS duplicate_rows
-FROM `dotted-forest-437921-f3.Cyclistic.join_data`;
-
--- Remove duplicates from column ride_id
-
-SELECT DISTINCT(ride_id)
-FROM `dotted-forest-437921-f3.Cyclistic.join_data`
-ORDER BY ride_id;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+CREATE TABLE `dotted-forest-437921-f3.Cyclistic.cleaned_data` AS (
+  SELECT ride_id
+    ,rideable_type
+    ,started_at
+    ,ended_at
+    ,ride_length_in_mins
+    ,CASE 
+    EXTRACT(MONTH FROM started_at)
+      WHEN 1 THEN 'January'
+      WHEN 2 THEN 'February'
+      WHEN 3 THEN 'March'
+      WHEN 4 THEN 'April'
+      WHEN 5 THEN 'May'      
+      WHEN 6 THEN 'June'
+      WHEN 7 THEN 'July'
+      WHEN 8 THEN 'August'
+      WHEN 9 THEN 'September'
+      WHEN 10 THEN 'October'      
+      WHEN 11 THEN 'November'
+      WHEN 12 THEN 'December'
+    END AS month
+    ,CASE
+    EXTRACT(DAYOFWEEK FROM started_at)
+      WHEN 1 THEN 'Sunday'
+      WHEN 2 THEN 'Monday'
+      WHEN 3 THEN 'Tuesday'
+      WHEN 4 THEN 'Wednesday'
+      WHEN 5 THEN 'Thursday'
+      WHEN 6 THEN 'Friday'
+      WHEN 7 THEN 'Saturday'    
+    END AS day_of_week
+    ,start_station_name
+    ,start_station_id
+    ,end_station_name
+    ,end_station_id
+    ,start_lat
+    ,start_lng
+    ,end_lat
+    ,end_lng
+    ,member_casual
+FROM `dotted-forest-437921-f3.Cyclistic.join_data` AS t1
+JOIN 
+  (SELECT ride_id
+    ,TIMESTAMP_DIFF(
+      ended_at
+      ,started_at
+      ,MINUTE) AS ride_length_in_mins
+  FROM `dotted-forest-437921-f3.Cyclistic.join_data`) AS t2
+USING(ride_id)
+WHERE ride_length_in_mins > 1 AND ride_length_in_mins < 1440 AND
+  start_station_name IS NOT NULL AND
+  start_station_id IS NOT NULL AND
+  end_station_name IS NOT NULL AND
+  end_station_id IS NOT NULL AND
+  start_lat IS NOT NULL AND
+  start_lng IS NOT NULL AND
+  end_lat IS NOT NULL AND
+  end_lng IS NOT NULL
+GROUP BY ride_id
+  ,rideable_type
+  ,started_at
+  ,ended_at
+  ,ride_length_in_mins
+  ,start_station_name
+  ,start_station_id
+  ,end_station_name
+  ,end_station_id
+  ,start_lat
+  ,start_lng
+  ,end_lat
+  ,end_lng
+  ,member_casual
+HAVING COUNT(ride_id) = 1
+);
