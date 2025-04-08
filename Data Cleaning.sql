5f081915b1e1e892105f5139d6b11ace1fbe3387
Olha Matvieieva <162842375+OlhaMatvieieva@users.noreply.github.com>
2025-04-08 11:53:22 +0200

    Update Data Cleaning.sql

--- Data Cleaning.sql
+++ Data Cleaning.sql
@@ -6,52 +6,11 @@
 -- determine the ride length in minutes 
 
 CREATE TABLE `dotted-forest-437921-f3.Cyclistic.cleaned_data` AS (
-  SELECT ride_id
-    ,rideable_type
-    ,started_at
-    ,ended_at
-    ,ride_length_in_mins
-    ,CASE 
-    EXTRACT(MONTH FROM started_at)
-      WHEN 1 THEN 'January'
-      WHEN 2 THEN 'February'
-      WHEN 3 THEN 'March'
-      WHEN 4 THEN 'April'
-      WHEN 5 THEN 'May'      
-      WHEN 6 THEN 'June'
-      WHEN 7 THEN 'July'
-      WHEN 8 THEN 'August'
-      WHEN 9 THEN 'September'
-      WHEN 10 THEN 'October'      
-      WHEN 11 THEN 'November'
-      WHEN 12 THEN 'December'
-    END AS month
-    ,CASE
-    EXTRACT(DAYOFWEEK FROM started_at)
-      WHEN 1 THEN 'Sunday'
-      WHEN 2 THEN 'Monday'
-      WHEN 3 THEN 'Tuesday'
-      WHEN 4 THEN 'Wednesday'
-      WHEN 5 THEN 'Thursday'
-      WHEN 6 THEN 'Friday'
-      WHEN 7 THEN 'Saturday'    
-    END AS day_of_week
-    ,start_station_name
-    ,start_station_id
-    ,end_station_name
-    ,end_station_id
-    ,start_lat
-    ,start_lng
-    ,end_lat
-    ,end_lng
-    ,member_casual
+  SELECT ride_id, rideable_type, started_at, ended_at, ride_length_in_mins, FORMAT_TIMESTAMP('%B', started_at) AS month, FORMAT_TIMESTAMP('%A', started_at) AS day_of_week, start_station_name, start_station_id, end_station_name, end_station_id, start_lat, start_lng, end_lat, end_lng, member_casual
 FROM `dotted-forest-437921-f3.Cyclistic.join_data` AS t1
 JOIN 
-  (SELECT ride_id
-    ,TIMESTAMP_DIFF(
-      ended_at
-      ,started_at
-      ,MINUTE) AS ride_length_in_mins
+  (SELECT ride_id, TIMESTAMP_DIFF(
+      ended_at, started_at, MINUTE) AS ride_length_in_mins
   FROM `dotted-forest-437921-f3.Cyclistic.join_data`) AS t2
 USING(ride_id)
 WHERE ride_length_in_mins > 1 AND ride_length_in_mins < 1440 AND
@@ -63,19 +22,6 @@ WHERE ride_length_in_mins > 1 AND ride_length_in_mins < 1440 AND
   start_lng IS NOT NULL AND
   end_lat IS NOT NULL AND
   end_lng IS NOT NULL
-GROUP BY ride_id
-  ,rideable_type
-  ,started_at
-  ,ended_at
-  ,ride_length_in_mins
-  ,start_station_name
-  ,start_station_id
-  ,end_station_name
-  ,end_station_id
-  ,start_lat
-  ,start_lng
-  ,end_lat
-  ,end_lng
-  ,member_casual
+GROUP BY ride_id, rideable_type, started_at, ended_at, ride_length_in_mins, start_station_name, start_station_id, end_station_name, end_station_id, start_lat, start_lng, end_lat, end_lng, member_casual
 HAVING COUNT(ride_id) = 1
 );
